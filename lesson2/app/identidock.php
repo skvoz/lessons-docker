5fd03fdcaeb16c9bd77e1c40c22c1f7df850889f
skvoz <skvoz.ne@gmail.com>
2019-08-07 11:07:03 +0300

    tmp commit

--- lesson2/app/identidock.php
+++ lesson2/app/identidock.php
@@ -1,5 +1,9 @@
 <?php
 
+echo 'Hello world';
+echo PHP_EOL;
+var_dump(file_exists(__DIR__.'/vendor/autoload.php'));
+//echo 222;
 require_once __DIR__.'/vendor/autoload.php';
 
 $app = new Silex\Application();
@@ -7,7 +11,6 @@ $app = new Silex\Application();
 
 $app->get('/', function($name) use($app) {
     echo  'Hello world!';
-    die;
 });
 
 $app->run();
