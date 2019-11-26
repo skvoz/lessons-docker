401241d43135f77d590973fb036c3beff301d052
skvoz <skvoz.ne@gmail.com>
2019-11-26 11:24:39 +0200

    some change lessons

--- lesson6/cmd.sh
+++ lesson6/cmd.sh
@@ -6,6 +6,9 @@ PHP=php
 if [ "$ENV" = 'DEV' ]; then
     echo "Running Development Server"
     $PHP -S 0.0.0.0:5000 $(pwd)/index.php
+elif [ "$ENV" = 'UNIT' ]; then
+    echo "Running Unit Tests"
+    $PHP /app/vendor/bin/phpunit
 else
     echo "Running Production Server"
     $PHP -S 0.0.0.0:6000 $(pwd)/index.php
